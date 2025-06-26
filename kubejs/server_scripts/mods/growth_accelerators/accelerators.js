2beb90aba44e7b3d467cd442548f25dad37c24c1
oly2o6 <calumroberts02@hotmail.com>
2025-06-26 22:01:00 +0100

    Boosted and directional growth accelerators

--- kubejs/server_scripts/mods/growth_accelerators/accelerators.js
+++ kubejs/server_scripts/mods/growth_accelerators/accelerators.js
@@ -13,4 +13,30 @@ ServerEvents.recipes(allthemods => {
         D: 'minecraft:cobblestone'
     })
 
+    allthemods.shaped('growthacceleratortiers:directional_growth_accelerator', [
+        'ECE',
+        'BAB',
+        'DFD'
+    ], {
+        A: 'ae2:growth_accelerator',
+        B: 'minecraft:diamond',
+        C: 'ae2:fluix_pearl',
+        D: 'minecraft:netherite_ingot',
+        E: 'ae2:speed_card',
+        F: 'ae2:formation_core'
+    })
+
+    allthemods.shaped('growthacceleratortiers:boosted_growth_accelerator', [
+        'ECE',
+        'BAB',
+        'DFD'
+    ], {
+        A: 'ae2:growth_accelerator',
+        B: 'minecraft:emerald',
+        C: 'ae2:fluix_pearl',
+        D: 'minecraft:netherite_ingot',
+        E: 'ae2:speed_card',
+        F: 'ae2:annihilation_core'
+    })
+
     })
