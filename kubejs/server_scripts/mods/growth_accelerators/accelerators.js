eeb52565abe3addf7a55a3ea1e99ebb18f0d96a6
oly2o6 <calumroberts02@hotmail.com>
2025-06-26 01:33:48 +0100

    Cranked accelerator

---
+++ kubejs/server_scripts/mods/growth_accelerators/accelerators.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes(allthemods => {
+
+    // cranked growth accelerator
+
+    allthemods.shaped('growthacceleratortiers:cranked_growth_accelerator', [
+        'DCD',
+        'BAB',
+        'DCD'
+    ], {
+        A: '#minecraft:planks',
+        B: 'minecraft:iron_ingot',
+        C: 'ae2:crank',
+        D: 'minecraft:cobblestone'
+    })
+
+    })
