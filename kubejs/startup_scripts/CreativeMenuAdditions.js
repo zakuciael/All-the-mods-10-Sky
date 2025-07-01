b99cb060e90d9081b6e9e0402d6541e4772fbd54
Satherov <Satherov@users.noreply.github.com>
2025-07-01 18:04:43 +0200

    feat: compact machines recipes + creative menu additions

--- kubejs/startup_scripts/CreativeMenuAdditions.js
+++ kubejs/startup_scripts/CreativeMenuAdditions.js
@@ -1,23 +1,7 @@
 ﻿// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-const $CreatingFluxRecipe = Java.loadClass('sonar.fluxnetworks.client.jei.CreatingFluxRecipe')
-const $CreatingFluxRecipeCategory = Java.loadClass('sonar.fluxnetworks.client.jei.CreatingFluxRecipeCategory')
-
-RecipeViewerEvents.addEntries('item', allthemods =>{
-
-    let jeiRuntime = global.jeiRuntime
-    if(!jeiRuntime) {
-        console.log('Jei Runtime not found, skipping recipes')
-        return
-    }
-
-    let RecipeManager = jeiRuntime.getRecipeManager()
-    global.flux.forEach(recipe => {
-        let customFlux = new $CreatingFluxRecipe(recipe.baseBlock, recipe.clickedBlock, recipe.inputItem, recipe.outputItem)
-        RecipeManager.addRecipes($CreatingFluxRecipeCategory.RECIPE_TYPE, [customFlux])
-    })
-
+StartupEvents.modifyCreativeTab('compactmachines:main', allthemods => {
     allthemods.add('compactmachines:new_machine[compactmachines:machine_color="#C95B13",compactmachines:room_template="compactmachines:tiny"]')
     allthemods.add('compactmachines:new_machine[compactmachines:machine_color="#D4D2D2",compactmachines:room_template="compactmachines:small"]')
     allthemods.add('compactmachines:new_machine[compactmachines:machine_color="#FBF236",compactmachines:room_template="compactmachines:normal"]')
@@ -26,7 +10,9 @@ RecipeViewerEvents.addEntries('item', allthemods =>{
     allthemods.add('compactmachines:new_machine[compactmachines:machine_color="#43D6CD",compactmachines:room_template="compactmachines:giant"]')
     allthemods.add('compactmachines:new_machine[compactmachines:machine_color="#7B2FBE",compactmachines:room_template="compactmachines:soaryn"]')
     allthemods.add('compactmachines:new_machine[compactmachines:machine_color="#00FF00",compactmachines:room_template="compactmachines:farming"]')
+})
 
+StartupEvents.modifyCreativeTab('ironfurnaces:ironfurnaces_tab', allthemods => {
     allthemods.add('ironfurnaces:allthemodium_furnace')
     allthemods.add('ironfurnaces:vibranium_furnace')
     allthemods.add('ironfurnaces:unobtainium_furnace')
