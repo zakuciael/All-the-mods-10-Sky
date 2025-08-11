92196fc197e4247a8181817b1138520a06b140e4
oly2o6 <calumroberts02@hotmail.com>
2025-08-11 03:50:20 +0100

    Added dummy drop to geores

---
+++ kubejs/server_scripts/mods/geore/georesecondaries.js
@@ -0,0 +1,43 @@
+LootJS.modifiers((allthemods) => {
+
+  const materials = [
+    "coal",
+    "lapis",
+    "copper",
+    "iron",
+    "gold",
+    "redstone",
+    "diamond",
+    "emerald",
+    "platinum",
+    "ancient_debris",
+    "quartz",
+    "tin",
+    "uraninite",
+    "uranium",
+    "zinc",
+    "nickel",
+    "ruby",
+    "topaz",
+    "sapphire",
+    "silver",
+    "black_quartz",
+    "lead",
+    "aluminum",
+    "osmium"
+
+  ];
+
+for (const resource of materials) {
+    allthemods
+      .addBlockModifier(`geore:small_${resource}_bud`)
+      .addLoot(LootEntry.of('exdeorum:stone_pebble'));
+    allthemods
+      .addBlockModifier(`geore:medium_${resource}_bud`)
+      .addLoot(LootEntry.of('exdeorum:stone_pebble'));
+    allthemods
+      .addBlockModifier(`geore:large_${resource}_bud`)
+      .addLoot(LootEntry.of('exdeorum:stone_pebble'));
+  }
+
+});
