3d256188cdeb7708f117cbe6a0b44b000bc10b0d
oly2o6 <calumroberts02@hotmail.com>
2025-08-11 03:10:55 +0100

    Star recipe fixes

--- kubejs/server_scripts/mods/atm/creative.js
+++ kubejs/server_scripts/mods/atm/creative.js
@@ -11,18 +11,18 @@ ServerEvents.recipes(allthemods => {
         j: "allthecompressed:obsidian_5x",
         k: "allthecompressed:gravel_5x",
         l: "allthecompressed:sand_5x"
-      }).id('kubejs:allthetweaks/pulsating_black_hole')
+      })
 
    // Nexium Emitter
     allthemods.shaped('allthetweaks:nexium_emitter', ['A B', ' CF', 'GED'], {
         A: 'powah:player_transmitter_nitro',
-        B: [Item.of('ae2wtlib:wireless_universal_terminal', '{crafting:1b,internalMaxPower:4800000.0d,pattern_access:1b,pattern_encoding:1b}')],
-        F: [Item.of('mekanism:module_gravitational_modulating_unit')],
+        B: 'ae2wtlib:wireless_universal_terminal',
+        F: 'mekanism:module_gravitational_modulating_unit',
         D: 'create:mechanical_arm',
         E: 'ae2:singularity',
         C: 'xnet:wireless_router',
         G: 'oritech:superconductor'
-      }).id('kubejs:allthetweaks/nexium_emitter')
+      })
 
   // Dragon Soul
   allthemods.shaped('allthetweaks:dragon_soul', ['CDA', 'XNY', 'BGE'], {
@@ -35,20 +35,19 @@ ServerEvents.recipes(allthemods => {
     E: 'ars_nouveau:summon_focus',
     X: 'draconicevolution:chaotic_core',
     Y: 'draconicevolution:chaotic_energy_core'
-  }).id('kubejs:allthetweaks/dragon_soul')
+  })
 
     // Philosopher's Fuel
-    allthemods.shaped('allthetweaks:philosophers_fuel', ['ABC', 'DEF', 'GHI'], {
+    allthemods.shaped('allthetweaks:philosophers_fuel', ['ABC', 'DEF', 'IHI'], {
         A: 'generatorgalore:ender_generator',
         B: 'ironfurnaces:unobtainium_furnace',
         C: 'justdirethings:coalblock_t4',
-        D: 'actuallyadditions:empowered_oil_bucket',
+        D: 'actuallyadditions:empowered_canola_seed',
         E: 'mysticalagradditions:insanium_coal_block',
         F: 'forbidden_arcanus:deorum_block',
-        G: 'mysticalagriculture:awakened_supremium_furnace',
         H: 'draconicevolution:reactor_core',
         I: 'enderio:sentient_ender'
-      }).id('kubejs:allthetweaks/philosopher_fuel')
+      })
 
   // Improbable Probability Device
   allthemods.shaped('allthetweaks:improbable_probability_device', ['AFA', 'CFE', 'BFD'], {
@@ -57,24 +56,24 @@ ServerEvents.recipes(allthemods => {
     C: 'megacells:cell_component_16m',
     D: 'oritech:large_storage_block',
     E: 'rftoolsutility:flight_module',
-    F: Item.of('powah:battery_nitro', '{powah_tile_data:{energy_stored_main_energy:2000000000L}}').weakNBT()
-    }).id('kubejs:allthetweaks/improbable_probability_device')
+    F: 'powah:battery_nitro[powah:energy_stored=2000000000L]'
+    })
 
     // Withers Compass
     allthemods.shaped('allthetweaks:withers_compass', ['DCD', 'ABA', 'DED'], {
         A: 'generatorgalore:netherstar_generator_8x',
         B: 'industrialforegoing:wither_builder',
-        C: Item.of('hostilenetworks:prediction', '{data_model:{id:"hostilenetworks:wither"}}').strongNBT(),
+        C: 'hostilenetworks:prediction[hostilenetworks:data_model="hostilenetworks:wither"]',
         D: 'reliquary:witherless_rose',
         E: 'actuallyadditions:player_interface'
-      }).id('kubejs:allthetweaks/withers_compass')
+      })
 
   // Oblivion Shard
   allthemods.shaped('allthetweaks:oblivion_shard', [' AB', 'ACA', 'BA '], {
     A: 'ae2:quantum_entangled_singularity',
     C: 'oritech:nuke',
     B: 'justdirethings:paradoxmachine'
-  }).id('kubejs:allthetweaks/oblivion_shard')
+  })
 
 
 //
