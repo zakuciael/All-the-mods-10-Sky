7b5a23cf5ddca555105672a228e2289fc5ba0457
Satherov <satherov@users.noreply.github.com>
2025-08-20 20:32:05 +0200

    soul lava addition

--- kubejs/server_scripts/mods/industrial_foregoing/laser_drill.js
+++ kubejs/server_scripts/mods/industrial_foregoing/laser_drill.js
@@ -39,12 +39,58 @@ ServerEvents.recipes(allthemods => {
             })
             .id(`allthemods:industrialforegoing/laser_drill_ore/${output.tag.split(':')[1]}`);
     }
+
+    /**
+     * @param {{fluid: string, amount: number}} output
+     * @param {string} entity
+     * @param {string} catalyst
+     * @param {number} depthMin
+     * @param {number} depthMax
+     * @param {number} weight
+     */
+    function laserDrillFluid(output, entity, catalyst, depthMin, depthMax, weight) {
+        allthemods
+            .custom({
+                type: 'industrialforegoing:laser_drill_fluid',
+                output: {
+                    id: output.fluid,
+                    amount: output.amount || 1
+                },
+                entity: entity || "minecraft:empty",
+                rarity: [
+                    {
+                        biome_filter: {
+                            whitelist:  [],
+                            blacklist:  [],
+                        },
+                        dimension_filter: {
+                            whitelist:  [],
+                            blacklist:  [],
+                        },
+                        depth_min:  depthMin,
+                        depth_max:  depthMax,
+                        weight:     weight
+                    }
+                ],
+                catalyst: {
+                    item: catalyst
+                }
+            })
+            .id(`allthemods:industrialforegoing/laser_drill_fluid/${output.fluid.split(':')[1]}`);
+    }
     
     laserDrillOre(
         {tag: 'c:gems/benitoite', count: 8},
         'industrialforegoing:blue_laser_lens',
         5, 256, 5
     );
+
+    laserDrillFluid(
+        {fluid: 'allthemodium:soul_lava', amount: 10},
+        'minecraft:warden',
+        'industrialforegoing:blue_laser_lens',
+        -64, 80, 3
+    )
     
 })
 
