834112eb899868aae716aaa62fa3a6aca2c4953a
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2025-08-24 18:18:25 -0300

    feat: add Actually Additions Schemas and recipe examples

---
+++ kubejs/server_scripts/mods/actuallyadditions/recipes.js
@@ -0,0 +1,27 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+    const aa = allthemods.recipes.actuallyadditions
+    /*
+    aa.coffee_ingredient("#ae2:all_fluix", 2, [{effect: "minecraft:regeneration", amplifier: 0, duration: 60}], "Some Extra Text here?")
+    aa.color_change("acacia_door", "#actuallyadditions:lamps")
+    // aa.copy_nbt()
+    // aa.copy_nbt_shapeless()
+    aa.crushing([{stack: "3x acacia_boat", chance: 1}, {stack: "apple", chance: 0.5}], "#ae2:metal_ingots")
+    aa.empowering("acacia_boat",
+        "#actuallyadditions:tiny_coals",
+        ["#actuallyadditions:crystal_blocks", "#ae2:all_fluix", "#alltheores:ore_hammers", "#c:ingots"],
+        1234,
+        0xFF4414,
+        1234)
+    aa.fermenting("allthemodium:soul_lava", "milk", 200)
+    aa.laser("acacia_boat", "#actuallyadditions:coffee_beans", 400)
+    aa.liquid_fuel("milk", 400, 400)
+    aa.mining_lens("acacia_button", "#ae2:all_quartz", 30)
+    aa.pressing("actuallyadditions:canola_oil", "#actuallyadditions:crystals")
+    */
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
