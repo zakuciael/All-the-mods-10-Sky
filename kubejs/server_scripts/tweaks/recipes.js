032e0284c9261be7702fb8835199d18aa4f558ba
Eric Sieben <business@esieben.net>
2025-08-30 19:08:14 +0000

    Removed Recipes for MI shards in [Recipe Issue]: Tungsten Geore remnants remaining in the game Fixes #401

--- kubejs/server_scripts/tweaks/recipes.js
+++ kubejs/server_scripts/tweaks/recipes.js
@@ -250,7 +250,10 @@ ServerEvents.recipes(allthemods => {
             '#c:storage_blocks/amethyst'
         ]
     )
+  allthemods.remove({ id: /geore:.*tungsten_shard.*/ });
+  allthemods.remove({ id: /geore:.*monazite_shard.*/ });
 })
+
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.  
