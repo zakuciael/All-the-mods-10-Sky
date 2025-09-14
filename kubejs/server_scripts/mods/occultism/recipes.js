1047eaf314445862d891999b45137690e9a6420e
DivineFinal <vidabence6@gmail.com>
2025-09-14 18:04:54 +0200

    Honeycomb Ritual + Apprentice Satchel Upgrade

--- kubejs/server_scripts/mods/occultism/recipes.js
+++ kubejs/server_scripts/mods/occultism/recipes.js
@@ -24,6 +24,82 @@ ServerEvents.recipes(allthemods => {
         crushing_time: 100,
         ignore_crushing_multiplier: true
     }).id('allthemods:occultism/crushing/coal_dust_from_shard')
+
+	allthemods.custom({
+  "type": "occultism:ritual",
+  "activation_item": {
+    "item": "occultism:book_of_binding_bound_foliot"
+  },
+  "duration": 60,
+  "ingredients": [
+    {
+      "tag": "minecraft:flowers"
+    },
+	{
+      "tag": "c:campfires"
+    },
+	{
+      "tag": "c:tools/shear"
+    },
+	{
+      "tag": "productivebees:solitary_overworld_nests"
+    },
+    {
+      "item": "minecraft:sugar"
+    },
+    {
+      "item": "minecraft:birch_sapling"
+    }
+  ],
+  "pentacle_id": "occultism:craft_foliot",
+  "result": {
+    "count": 3,
+    "id": "minecraft:honeycomb"
+  },
+  "ritual_dummy": {
+    "count": 1,
+    "id": "kubejs:ritual_dummy/honeycomb"
+  },
+  "ritual_type": "occultism:craft"
+}).id("allthemods:occultism/ritual/honeycomb")
+
+	allthemods.custom({
+  "type": "occultism:ritual",
+  "activation_item": {
+    "item": "occultism:book_of_binding_bound_afrit"
+  },
+  "duration": 300,
+  "ingredients": [
+    {
+      "item": "occultism:ritual_satchel_t1"
+    },
+    {
+      "item": "occultism:afrit_essence"
+    },
+    {
+      "tag": "c:ender_pearls"
+    },
+    {
+      "tag": "c:ender_pearls"
+    },
+    {
+      "tag": "c:ender_pearls"
+    },
+    {
+      "tag": "c:ender_pearls"
+    }
+  ],
+  "pentacle_id": "occultism:craft_afrit",
+  "result": {
+    "count": 1,
+    "id": "occultism:ritual_satchel_t2"
+  },
+  "ritual_dummy": {
+    "count": 1,
+    "id": "kubejs:ritual_dummy/upgrade_ritual_satchel_t1_to_t2"
+  },
+  "ritual_type": "occultism:craft_with_spirit_name"
+	}).id("allthemods:occultism/ritual/upgrade_ritual_satchel_t1_to_t2")
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
