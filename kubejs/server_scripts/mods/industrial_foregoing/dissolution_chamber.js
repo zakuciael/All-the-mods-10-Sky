2c06a69d49e46abac6acb4718e1fec0ee7a89745
DivineFinal <vidabence6@gmail.com>
2025-09-14 06:02:05 +0200

    Make Bottle o' Enchanting Require a bottle in the Dissolution Chamber

---
+++ kubejs/server_scripts/mods/industrial_foregoing/dissolution_chamber.js
@@ -0,0 +1,53 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+
+    function dissolution_chamber(output, inputs, fluid, time, id) {
+        let recipe = {
+            "type": "industrialforegoing:dissolution_chamber",
+            "input": [],
+            "inputFluid": {
+                "amount": fluid.amount || 100,
+                "fluid": fluid.fluid
+            },
+            "output": {
+                "count": output.count || 1,
+                "id": output.item
+            },
+            "processingTime": time
+        };
+
+        inputs.forEach(input => {
+
+            let ingredients = {}
+
+            if (input.tag) {
+                ingredients.tag = input.tag;
+            } else {
+                ingredients.item = input.item;
+            }
+
+            recipe.input.push(ingredients);
+        });
+
+        allthemods.custom(recipe).id(`kubejs:dissolution_chamber/${id}`);
+    }
+
+
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/xp_bottles'})
+    dissolution_chamber(
+        {item: 'minecraft:experience_bottle'},
+        [
+            {item: 'minecraft:glass_bottle'}],
+        {
+            fluid: 'industrialforegoing:essence',
+            amount: 250
+        },
+        5,
+        'xp_bottle'
+    );
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
