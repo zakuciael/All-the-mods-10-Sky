8648d6c524202301495cee4eba160e107b45b415
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-09-19 11:55:02 -0500

    Added Missing Metalworks casting Recipes

---
+++ kubejs/server_scripts/mods/metalworks/block_casting_recipes.js
@@ -0,0 +1,26 @@
+ServerEvents.recipes(event => 
+    {
+        // Map of block casting recipes
+        const blockCastingItems = [
+            { name: 'coal', id: 'minecraft:coal_block', fluid: 'productivemetalworks:molten_carbon', amount: 810 },
+            { name: 'ancient_debris', id: 'minecraft:ancient_debris', fluid: 'productivemetalworks:molten_ancient_debris', amount: 200 },
+            { name: 'quartz', id: 'minecraft:quartz_block', fluid: 'productivemetalworks:molten_quartz', amount: 400 }
+        ]
+
+        blockCastingItems.forEach(item => {
+            event.custom({
+            type: 'productivemetalworks:block_casting',
+            cast: [],
+            consume_cast: false,
+            fluid: {
+                amount: item.amount,
+                fluid: item.fluid
+            },
+            result: {
+                count: 1,
+                id: item.id
+            }
+            }).id(`allthemods:metalworks/casting/block/${item.name}`)
+        })
+    }
+)
