2632c413925254540253220265c4c6b6f40f40cb
doyle31 <abacubabacus@gmail.com>
2025-09-22 13:28:15 +1000

    add MA seeds to the extended industrialization farmer whitelist

---
+++ kubejs/server_scripts/mods/extendedindustrialization/farmer.js
@@ -0,0 +1,53 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.tags("item", (allthemods) => {
+    
+    allthemods.add("extended_industrialization:farmer_plantable", [
+        'mysticalagriculture:inferium_seeds',
+        'mysticalagriculture:dirt_seeds',
+        'mysticalagriculture:wood_seeds',
+        'mysticalagriculture:ice_seeds',
+        'mysticalagriculture:air_seeds',
+        'mysticalagriculture:earth_seeds',
+        'mysticalagriculture:water_seeds',
+        'mysticalagriculture:fire_seeds',
+        'mysticalagriculture:stone_seeds',
+        'mysticalagriculture:deepslate_seeds',
+        'mysticalagriculture:nature_seeds',
+        'mysticalagriculture:dye_seeds',
+        'mysticalagriculture:nether_seeds',
+        'mysticalagriculture:coral_seeds',
+        'mysticalagriculture:honey_seeds',
+        'mysticalagriculture:chicken_seeds',
+        'mysticalagriculture:cow_seeds',
+        'mysticalagriculture:sheep_seeds',
+        'mysticalagriculture:squid_seeds',
+        'mysticalagriculture:fish_seeds',
+        'mysticalagriculture:slime_seeds',
+        'mysticalagriculture:turtle_seeds',
+        'mysticalagriculture:armadillo_seeds',
+        'mysticalagriculture:limestone_seeds',
+        'mysticalagriculture:basalt_seeds',
+        'mysticalagriculture:skeleton_seeds',
+        'mysticalagriculture:creeper_seeds',
+        'mysticalagriculture:spider_seeds',
+        'mysticalagriculture:phantom_seeds',
+        'mysticalagriculture:rabbit_seeds',
+        'mysticalagriculture:experience_seeds',
+        'mysticalagriculture:breeze_seeds',
+        'mysticalagriculture:blaze_seeds',
+        'mysticalagriculture:ghast_seeds',
+        'mysticalagriculture:enderman_seeds',
+        'mysticalagriculture:soulium_seeds',
+        'mysticalagriculture:wither_skeleton_seeds',
+        'mysticalagriculture:nether_star_seeds',
+        'mysticalagriculture:dragon_egg_seeds',
+        'mysticalagriculture:awakened_draconium_seeds',
+        
+    ])
+})
+
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
