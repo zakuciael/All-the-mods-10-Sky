eb361ac0cd8925fcc189f0bdcd92e40de834e08d
klaborda <98995+klaborda@users.noreply.github.com>
2025-09-22 19:54:33 +0000

    chore: prettier formatting

--- kubejs/server_scripts/mods/draconic_evolution/alloy_sword.js
+++ kubejs/server_scripts/mods/draconic_evolution/alloy_sword.js
@@ -1,40 +1,40 @@
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-ServerEvents.recipes(allthemods => {
-    allthemods
+ServerEvents.recipes((allthemods) => {
+  allthemods
     .custom({
-        "type": "draconicevolution:fusion_crafting",
-        "catalyst": {
-          "item": "minecraft:netherite_sword"
-        },
-        "ingredients": [
-          {
-            "consume": true,
-            "ingredient": {
-              "tag": "c:ingots/allthemodium"
-            }
-          },
-          {
-            "consume": true,
-            "ingredient": {
-              "tag": "c:ingots/vibranium"
-            }
-          },
-          {
-            "consume": true,
-            "ingredient": {
-              "tag": "c:ingots/unobtainium"
-            }
+      type: "draconicevolution:fusion_crafting",
+      catalyst: {
+        item: "minecraft:netherite_sword"
+      },
+      ingredients: [
+        {
+          consume: true,
+          ingredient: {
+            tag: "c:ingots/allthemodium"
           }
-        ],
-        "result": {
-          "count": 1,
-          "id": "allthemodium:alloy_sword"
         },
-        "techLevel": "draconic",
-        "totalEnergy": 32000000
-      })
+        {
+          consume: true,
+          ingredient: {
+            tag: "c:ingots/vibranium"
+          }
+        },
+        {
+          consume: true,
+          ingredient: {
+            tag: "c:ingots/unobtainium"
+          }
+        }
+      ],
+      result: {
+        count: 1,
+        id: "allthemodium:alloy_sword"
+      },
+      techLevel: "draconic",
+      totalEnergy: 32000000
+    })
     .id("allthemodsmodium:draconic_evolution/alloy_sword")
 })
 
