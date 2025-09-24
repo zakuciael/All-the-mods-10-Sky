8b2186ffefc9357706a8eefc571f1ca483a5dde9
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-09-24 07:36:53 +0000

    chore: prettier formatting

--- kubejs/server_scripts/mods/enderio/recipes.js
+++ kubejs/server_scripts/mods/enderio/recipes.js
@@ -1,22 +1,21 @@
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-ServerEvents.recipes(allthemods => {
-
-    allthemods.custom({
-    "type": "enderio:vat_fermenting",
-    "input": {
-        "amount": 1000,
-        "tag": "minecraft:lava"
+ServerEvents.recipes((allthemods) => {
+  allthemods.custom({
+    type: "enderio:vat_fermenting",
+    input: {
+      amount: 1000,
+      tag: "minecraft:lava"
     },
-    "left_reagent": "exdeorum:compressed/soul_sand",
-    "output": {
-        "amount": 100,
-        "id": "allthemodium:soul_lava"
+    left_reagent: "exdeorum:compressed/soul_sand",
+    output: {
+      amount: 100,
+      id: "allthemodium:soul_lava"
     },
-    "right_reagent": "allthemods:tendrils/warden",
-    "ticks": 200
-    })
+    right_reagent: "allthemods:tendrils/warden",
+    ticks: 200
+  })
 })
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
