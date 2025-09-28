7fbf3a98b91ca0aa97c2eedc2474029f4494b176
doyle31 <128433752+doyle31@users.noreply.github.com>
2025-09-28 18:09:10 +0000

    chore: prettier formatting

--- kubejs/server_scripts/mods/ExtendedAE/InfinityCellRecipes.js
+++ kubejs/server_scripts/mods/ExtendedAE/InfinityCellRecipes.js
@@ -71,20 +71,8 @@ ServerEvents.recipes((allthemods) => {
     "mekanism:crusher",
     "extendedae:infinity_cobblestone_cell"
   )
-  cell(
-    "kubejs:sand_cell",
-    "allthecompressed:sand_6x",
-    "mekanism:crusher",
-    "mekanism:crusher",
-    "kubejs:gravel_cell"
-  )
-  cell(
-    "kubejs:dust_cell",
-    "allthecompressed:dust_6x",
-    "mekanism:crusher",
-    "mekanism:crusher",
-    "kubejs:sand_cell"
-  )
+  cell("kubejs:sand_cell", "allthecompressed:sand_6x", "mekanism:crusher", "mekanism:crusher", "kubejs:gravel_cell")
+  cell("kubejs:dust_cell", "allthecompressed:dust_6x", "mekanism:crusher", "mekanism:crusher", "kubejs:sand_cell")
   cell(
     "kubejs:red_sand_cell",
     "allthecompressed:red_sand_5x",
