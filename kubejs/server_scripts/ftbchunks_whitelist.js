0b24388b13ea5283b2eeaa708665166675dbee55
billbertking1 <1744629+billbertking1@users.noreply.github.com>
2025-10-01 21:26:45 +0000

    chore: prettier formatting

--- kubejs/server_scripts/ftbchunks_whitelist.js
+++ kubejs/server_scripts/ftbchunks_whitelist.js
@@ -1,4 +1,4 @@
-ServerEvents.tags('block', event => {
-    event.add('ftbchunks:interact_whitelist', 'epitaphs:grave')
-    event.add('ftbchunks:edit_whitelist', 'epitaphs:grave')
+ServerEvents.tags("block", (event) => {
+  event.add("ftbchunks:interact_whitelist", "epitaphs:grave")
+  event.add("ftbchunks:edit_whitelist", "epitaphs:grave")
 })
