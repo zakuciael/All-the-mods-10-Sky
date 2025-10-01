12804b5acccfaf7148251fd646b7e0deee5703eb
doyle31 <abacubabacus@gmail.com>
2025-10-02 08:02:47 +1000

    add atm alloy ingots to c:ingots, fixes #131

--- kubejs/server_scripts/tweaks/tags.js
+++ kubejs/server_scripts/tweaks/tags.js
@@ -15,6 +15,12 @@ ServerEvents.tags("block", (allthemods) => {
     "@waystones"
   ])
 
+  allthemods.add("c:ingots", [
+    'allthemodium:unobtainium_allthemodium_alloy_ingot',
+    'allthemodium:unobtainium_vibranium_alloy_ingot',
+    'allthemodium:vibranium_allthemodium_alloy_ingot'
+  ])
+
   allthemods.remove("c:relocation_not_supported", [
     "geore:budding_diamond",
     "geore:budding_gold",
