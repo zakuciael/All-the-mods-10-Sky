4188da61a1b8dac4b46d8ae236d41fe356c12ccf
doyle31 <abacubabacus@gmail.com>
2025-10-02 10:09:29 +1000

    fix #472

---
+++ kubejs/server_scripts/mods/dyenamics/lavender.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes((allthemods) => {
+    allthemods.remove({ id: "dyenamics:ma/dye/lavender_dye" })
+    allthemods.shaped("dyenamics:lavender_dye", [
+        " D ",
+        "   ",
+        "D D"
+    ], {L: "dyenamics:lavender_dye" })
+})
