49b5b2fa97d0f5046e5d8ff77f851674c5ef07ff
DivineFinal <24657715+DivineFinal@users.noreply.github.com>
2025-10-02 20:46:44 +0000

    chore: prettier formatting

--- kubejs/server_scripts/mods/enderio/recipes.js
+++ kubejs/server_scripts/mods/enderio/recipes.js
@@ -17,36 +17,33 @@ ServerEvents.recipes((allthemods) => {
     ticks: 200
   })
   // Suspicious Seed Recipe
-  allthemods.custom({
-	"type": "enderio:fire_crafting",
-	"base_blocks": [
-		"allthecompressed:obsidian_1x",
-		"minecraft:bedrock"
-	],
-	"dimensions": [
-		"minecraft:overworld"
-	],
-	"results": [
-		{
-		"chance": 0.8,
-		"max_count": 3,
-		"min_count": 1,
-		"result": {
-			"count": 1,
-			"id": "enderio:grains_of_infinity"
-			}
-		},
-		{
-		"chance": 0.3,
-		"max_count": 1,
-		"min_count": 1,
-		"result": {
-			"count": 1,
-			"id": "enderio:suspicious_seed"
-			}
-		}
-	]
-	}).id("enderio:fire_crafting/bedrock_infinity")
+  allthemods
+    .custom({
+      type: "enderio:fire_crafting",
+      base_blocks: ["allthecompressed:obsidian_1x", "minecraft:bedrock"],
+      dimensions: ["minecraft:overworld"],
+      results: [
+        {
+          chance: 0.8,
+          max_count: 3,
+          min_count: 1,
+          result: {
+            count: 1,
+            id: "enderio:grains_of_infinity"
+          }
+        },
+        {
+          chance: 0.3,
+          max_count: 1,
+          min_count: 1,
+          result: {
+            count: 1,
+            id: "enderio:suspicious_seed"
+          }
+        }
+      ]
+    })
+    .id("enderio:fire_crafting/bedrock_infinity")
 })
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10: To the Sky.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
