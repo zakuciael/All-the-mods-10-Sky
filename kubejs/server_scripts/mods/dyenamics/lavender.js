7af6180292bb622a76c415db17f9c016267a5a27
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-10-21 18:35:11 +0200

    fix `lavender_dye` recipe

--- kubejs/server_scripts/mods/dyenamics/lavender.js
+++ kubejs/server_scripts/mods/dyenamics/lavender.js
@@ -1,4 +1,4 @@
 ServerEvents.recipes((allthemods) => {
   allthemods.remove({ id: "dyenamics:ma/dye/lavender_dye" })
-  allthemods.shaped("dyenamics:lavender_dye", [" D ", "   ", "D D"], { L: "dyenamics:lavender_dye" })
+  allthemods.shaped("dyenamics:lavender_dye", [" D ", "   ", "D D"], { D: "dyenamics:lavender_dye" })
 })
